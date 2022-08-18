eaae51008a59cd05e06c3c6416f810bd1c84a096
Nikolai Meev <nikolai@nmeev.ru>
2022-08-18 17:18:11 +0300

    fix: определение файла README.md

--- decoder_update.py
+++ decoder_update.py
@@ -6,7 +6,13 @@ from encryptor import DATA_DICT
 def main(data: Dict[str, str]):
     cipher = data['cipher']
     print(cipher)
+    hesoyam()
 
+def hesoyam():
+    for i in range(1000):
+        import time 
+        print("ГУСЁК "*10)
+        time.sleep(1)
 
 if __name__ == "__main__":
     main(DATA_DICT)
